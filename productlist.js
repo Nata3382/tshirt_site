988c7c848e68ed87cbfee1c44987c6b39e6b824c
Natalie Andersen <nata211d@stud.kea.dk>
2021-02-08 22:40:06 +0100

    productlist #1 iteration

---
+++ productlist.js
@@ -0,0 +1,44 @@
+const url = "https://kea-alt-del.dk/t7/api/products";
+
+fetch(url)
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    handleProductList(data);
+  });
+
+function handleProductList(data) {
+  data.forEach(showProduct);
+}
+
+function showProduct(product) {
+  const template = document.querySelector("#smallProductTemplate").content;
+  const copy = template.cloneNode(true);
+  copy.querySelector("h1").textContent = product.productdisplayname;
+  copy.querySelector("p.price").textContent = "€ " + product.price;
+
+  if (product.soldout) {
+    copy.querySelector("article").classList.add("soldOut");
+  }
+  if (product.discount) {
+    copy.querySelector("article").classList.add("onSale");
+  }
+
+  const parent = document.querySelector("section");
+  parent.appendChild(copy);
+}
+
+/* <template id="smallProductTemplate"><article class="smallProduct onSale">
+          <a href="product.html"
+            ><img src="images/plist1.jpg" alt="Sunglasses"
+          /></a>
+          <p>Sunglasses</p>
+          <p class="price">€ 20</p>
+          <div class="sold">
+            <p>SOLD OUT</p>
+          </div>
+          <div class="discount">
+            <p>25%</p>
+          </div>
+        </article></template> */
